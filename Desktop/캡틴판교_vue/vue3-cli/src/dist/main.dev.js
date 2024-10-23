f7552c53a60553fb841f792990716d9c4d5efd10
seulki <seulki@whitescan.com>
2024-10-23 10:33:30 +0900

    vue3 강의 끝

---
+++ Desktop/캡틴판교_vue/vue3-cli/src/dist/main.dev.js
@@ -0,0 +1,11 @@
+"use strict";
+
+var _vue = require("vue");
+
+var _App = _interopRequireDefault(require("./App.vue"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+// 뷰 라이브러리를 들고옴 
+(0, _vue.createApp)(_App["default"]).mount('#app');
+//# sourceMappingURL=main.dev.js.map
